67137592321dc6a8c3a43fde8e88d08ae51ff638
Shyngyskhan07 <shaymerdin@mail.ru>
2019-02-07 10:39:23 +0600

    1 st task

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("C: /Users/Чингиз/Desktop/PP2/Week2/Task1/input.txt");      //way where we read
+            string s = sr.ReadToEnd();                                 //copy to string
+            sr.Close();                                                //to close
+            int cnt = 0;                                               //new cnt
+            for(int i = 0; i < s.Length/2; i++)                        //we begin at 0 and end at half of length
+            {
+                if (s[i] != s[s.Length - i - 1])                       //to check elements for example:1 st and last
+                    cnt++;                                             //if we find not same letter counter ++
+            }
+            if (cnt > 0)
+                Console.WriteLine("No");                                //outputs "NO"
+            else
+            {
+                Console.WriteLine("Yes");                                //outputs "YES"
+            }
+
+        }
+    }
+}
