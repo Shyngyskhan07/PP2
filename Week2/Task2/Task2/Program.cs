f5684526e48b734b25dad34449f924c52854df71
Shyngyskhan07 <shaymerdin@mail.ru>
2019-02-07 11:33:32 +0600

    Task2

---
+++ Week2/Task2/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("C: /Users/Чингиз/Desktop/PP2/Week2/Task2/input.txt");
+            StreamWriter sw = new StreamWriter("C: /Users/Чингиз/Desktop/PP2/Week2/Task2/output.txt");
+            string s = sr.ReadToEnd();
+            sr.Close();
+            string[] arr = s.Split();
+            int[] array1 = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+                array1[i] = int.Parse(arr[i]);
+            
+            for (int i = 0; i < array1.Length; i++)
+            {
+                int Dividers = 0;
+                if (array1[i] == 1)
+                    Dividers++;
+
+                for (int j = 2; j <= array1[i] / 2; j++)
+                    if (array1[i] % j == 0)
+                        Dividers++;
+
+                if (Dividers == 0)
+                  sw.Write(array1[i] + " ");
+            }
+               sw.Close();
+        }
+    }
+}
