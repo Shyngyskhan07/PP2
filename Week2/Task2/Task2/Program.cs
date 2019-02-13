33f66efa399289189812c476509f960156e53285
Shyngyskhan07 <shaymerdin@mail.ru>
2019-02-13 23:16:50 +0600

    Added comments

--- Week2/Task2/Task2/Program.cs
+++ Week2/Task2/Task2/Program.cs
@@ -11,30 +11,30 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader("C: /Users/Чингиз/Desktop/PP2/Week2/Task2/input.txt");
-            StreamWriter sw = new StreamWriter("C: /Users/Чингиз/Desktop/PP2/Week2/Task2/output.txt");
-            string s = sr.ReadToEnd();
-            sr.Close();
-            string[] arr = s.Split();
-            int[] array1 = new int[arr.Length];
+            StreamReader sr = new StreamReader("C: /Users/Чингиз/Desktop/PP2/Week2/Task2/input.txt");             //To read from input
+            StreamWriter sw = new StreamWriter("C: /Users/Чингиз/Desktop/PP2/Week2/Task2/output.txt");            //To write to output
+            string s = sr.ReadToEnd();                                                                            //Reads sr
+            sr.Close();                                                                                             //Close sr
+            string[] arr = s.Split();                                                                               //massive saves by splitting
+            int[] array1 = new int[arr.Length];                                                                     //new array with length arr
 
-            for (int i = 0; i < arr.Length; i++)
-                array1[i] = int.Parse(arr[i]);
+            for (int i = 0; i < arr.Length; i++)            
+                array1[i] = int.Parse(arr[i]);                                                                      //converts to int each number
             
             for (int i = 0; i < array1.Length; i++)
             {
-                int Dividers = 0;
-                if (array1[i] == 1)
-                    Dividers++;
+                int Dividers = 0;                                                                                   //To count dividers each number
+                if (array1[i] == 1)                                                                                 //1 is not prime
+                    Dividers++;                                                                                     //add +1 to dividers
 
-                for (int j = 2; j <= array1[i] / 2; j++)
-                    if (array1[i] % j == 0)
-                        Dividers++;
+                for (int j = 2; j <= array1[i] / 2; j++)                                                            //runs from 2 to half of number
+                    if (array1[i] % j == 0)                                                     
+                        Dividers++;                                                                                 //add +1 to dividers
 
                 if (Dividers == 0)
-                  sw.Write(array1[i] + " ");
+                  sw.Write(array1[i] + " ");                                                                        //if Dividers 0 writes in output.txt
             }
-               sw.Close();
+               sw.Close();                                                                                          //close sw
         }
     }
 }
--- Week2/Task3/Task3/Program.cs
+++ Week2/Task3/Task3/Program.cs
@@ -9,34 +9,35 @@ namespace Task3
 {
     class Program
     {
-        static void PrintSpaces(int level)
+        static void PrintSpaces(int level)                                  //method to print spaces
         {
-            for (int i = 0; i < level; i++)
-                Console.Write("  ");
+            for (int i = 0; i < level; i++)                                 //rung from 0 to level
+                Console.Write("  ");                                        //Outputs spaces
         }
 
-        static void GetInfo(DirectoryInfo directory, int level)
+        static void GetInfo(DirectoryInfo directory, int level)            //Method with 2 parametres
         {
-            FileInfo[] files = directory.GetFiles();
-            DirectoryInfo[] directories = directory.GetDirectories();
+            FileInfo[] files = directory.GetFiles();                       //Massive files
+            DirectoryInfo[] directories = directory.GetDirectories();     //Massive directories
 
-            foreach (FileInfo file in files)
+            foreach (FileInfo file in files)                              //Check massive files
             {
-                PrintSpaces(level);
-                Console.WriteLine(file.Name);
+                PrintSpaces(level);                                       //calls method PritSpaces
+                Console.WriteLine(file.Name);                             //Outputs Name of File
             }
 
-            foreach (DirectoryInfo d in directories)
+            foreach (DirectoryInfo d in directories)                      //Check massive directories
             {
-                PrintSpaces(level);
-                Console.WriteLine(d.Name);
-                GetInfo(d, level + 1);
+                PrintSpaces(level);             //calls method PrintSpaces
+                Console.WriteLine(d.Name);        //Outputs Name of directory
+                GetInfo(d, level + 1);            //calls method GetInfo with level++
             }
         }
         static void Main(string[] args)
         {
-            DirectoryInfo d = new DirectoryInfo("C: /Users/Чингиз/Desktop/PP2/Week2");
-            GetInfo(d,0);
+            DirectoryInfo d = new DirectoryInfo("C: /Users/Чингиз/Desktop/PP2/Week2"); //Gets directory info
+            GetInfo(d,0);                          //calls method GetInfo with level 0
+            
         }
     }
 }
